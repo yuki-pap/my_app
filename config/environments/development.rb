c9af7b6ed655553572f19d5927fce252f47d64f3
yuki <iizuka0908@gmail.com>
2020-07-30 15:04:01 +0900

    Fix for production4

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,12 +28,12 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  #config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'localhost' 
+  host = 'localhost'
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
